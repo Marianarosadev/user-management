e3d926d9922ac71cdfc2d272533ab9f1d0b10f30
Marianarosadev <mary.oliveira0212@gmail.com>
2024-05-06 21:01:21 -0300

    config server

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/user-management",
   build: {
-    outDir: 'build',
+    outDir: "build",
     emptyOutDir: true,
     sourcemap: false
   }
